ae358ad14726708e724ce8dd1fd76a56b1d7acde
Alexander H Lee III <ahlee1026@gmail.com>
2019-10-23 19:05:25 -0400

    cleared a few left overs

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,23 +1,5 @@
 import React, { Component } from "react";
 
-//import React from "react";
-// import { prependOnceListener } from 'cluster';
-
-
-  // const move = ({ keyCode }) => {
-  //   console.log(keyCode);
-  //   if (!gameOver) {
-  //     if (keyCode === 37) {
-  //       movePlayer(-1);
-  //     } else if (keyCode === 39) {
-  //       movePlayer(1);
-  //     } else if (keyCode === 40) {
-  //       dropPlayer();
-  //     } else if (keyCode === 38) {
-  //       playerRotate(stage, 1);
-  //     }
-  //   }
-  // };
 
 
 class Card extends Component {
@@ -47,35 +29,16 @@ state = {
   scramblenumbers = () => {
       var temp = [];
     if  (this.state.cardlist.length < 10) {
-        //console.log("this is your prps length:: "+this.props.cards.length+" thisis your cards length:: "+this.state.cardlist.length)
-        //console.log("this is the right track")
         for (var i = 0; temp.length < this.props.cards.length; i++) {
           var x = Math.floor(Math.random() * 9);
-          
-          //var y  = x - 1;
-        //   console.log("this is your random num:: "+x);
-          //console.log(this.state.cardlist.includes(y));
           if (temp.includes(this.props.cards[x])===false) {
-            // let newState = { cardlist:[...this.state.cardlist, this.props.cards[x].id]}
             temp.push(this.props.cards[x]);
             
         }
-        //console.log(temp);
         this.setState({ cardlist: temp }, () => {
             // console.log(this.state.cardlist);
         });
     
-        //   console.log(this.props.cards[i].id);
-        //   if (this.props.cards[i].id===y) {
-        //     for (var j = 0; j < this.state.cardlist.length; j++) {
-        //         if (this.state.cardlist[j].id===this.props.cards[i].id) {
-        //             console.log("j is i");
-        //         }
-                
-        //     }
-        //     this.state.cardlist.push(this.props.cards[i].id);
-        //     console.log(this.state.cardlist);
-        //   }
         }
     }
   }
