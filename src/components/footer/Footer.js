75acba170ee097c8b3c42a491aef25f7488fa7d7
Michael Passow <passowmichael@yahoo.com>
2019-10-28 18:24:17 -0400

    added footer css | moved links in header to right side

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,9 @@
 import React from "react";
-// import Navbar from "react-bootstrap/Navbar";
-// import Nav from "react-bootstrap/Nav";
-
 
 function Footer(){
     return (
       <footer className="fixed-bottom"> 
-       <p className="text-center">NumberNauts &copy; 2019</p>
+       <p className="text-center bg-dark text-white p-3">NumberNauts &copy; 2019</p>
       </footer>
   )
   }
