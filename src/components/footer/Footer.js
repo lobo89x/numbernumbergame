d4cbf891331209523e8c27cab27243212d37405f
Michael Passow <passowmichael@yahoo.com>
2019-10-29 18:46:05 -0400

    re-positioned footer on botoom of the page | added moving animated background

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(){
     return (
       <footer className="fixed-bottom"> 
-       <p className="text-center bg-dark text-white p-3">NumberNauts &copy; 2019</p>
+       <p className="text-center bg-dark text-white p-3 m-0">NumberNauts &copy; 2019</p>
       </footer>
   )
   }
