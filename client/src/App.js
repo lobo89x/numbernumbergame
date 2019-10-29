db8209fe526b4fb56a576023ce80c122723a74b7
StevenDixonDev <stevedixon1128.dev@gmail.com>
2019-10-29 13:32:20 -0400

    added lobby component to react

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import Lobby from './lobby';
+import Socket from 'socket.io-client';
+
+// the url used for the connection to the server in development we use localhost on heroku we need to use /
+const socketUrl = (process.env.NODE_ENV === "development") ? "http://localhost:3001" : "/" 
 
 function Login(){
   return(
@@ -25,7 +30,27 @@ function Game(){
     <div>Game</div>
   )
 }
+function Lobby({socket, user}){
+  return(
+    // needs user and socket passed to it
+    <Lobby user={user} socket={socket}/>
+  )
+}
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      // holds the socket connection
+      socket: null,
+      user: null
+    }
+  }
+  componentDidMount(){
+    // if user is logged in then do connection
+    if(this.state.user){
+      this.setState({socket: socketIOClient.connect(socketUrl)});
+    }
+  }
   render() {
     return (
       <Router>
@@ -34,6 +59,7 @@ class App extends Component {
         <Route exact path = "/" component ={LandingPage} />
         <Route exact path = "/register" component ={Register} />
         <Route exact path = "/login" component ={Login} />
+        <Route exact path = "/lobby" component ={()=> Lobby(this.state)} />
         <Route exact path = "/game" component ={Game} />
         <Route path = "*" component = {Page404} />
       </Switch>
