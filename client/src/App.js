78113dd8639190d49e97c1571810e50f66edfee0
Michael Passow <passowmichael@yahoo.com>
2019-10-31 19:27:06 -0400

    added 404 styling

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
     };
   }
   componentDidMount() {
-    this.updateUserLogin("Anon"+ Math.floor(Math.random()*100000));
+    //this.updateUserLogin("Anon"+ Math.floor(Math.random()*100000));
     //this.getUser();
   }
 
--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,16 +1,17 @@
 import React from "react";
-import {Card} from "react-bootstrap";
-import {Button} from "react-bootstrap";
+import {Card, Button, Spinner} from "react-bootstrap";
+
 
 function LandingPage(){
   return(
 
-    <Card style={{width:"40rem"}} className="mx-auto mt-3 border border-success">
-  <Card.Header className="text-center"><h1>Welcome to NumberNauts</h1></Card.Header>
-  <Card.Body>
+    <Card style={{width:"40rem"}} className="mx-auto mt-3 border border-success border-10">
+  <Card.Header className="text-center bg-success"><h1> <Spinner animation="grow" variant="dark" /> Welcome to NumberNauts <Spinner animation="grow" variant="dark" />  </h1></Card.Header>
+  <Card.Body className="bg-dark">
     {/* <Card.Title className="text-white"><h2>Sign Up Below</h2></Card.Title> */}
     <Card.Text className="text-white">
         <p> FIRST THINGS FIRST! Create a Username and Password so we can remember you and save your Hi-Scores!</p>
+        <hr></hr>
         <br></br>
       <ul>
         <h2>How NumberNauts Works</h2>
@@ -24,8 +25,8 @@ function LandingPage(){
       <br>
       </br>
     </Card.Text>
-    <Button className="ml-4" href="Login" variant="success">Login Here</Button>
-    <Button className="float-right mr-4" href="Login" variant="success">Create Account</Button>
+    <Button className="ml-4" href="/Login" variant="success">Login Here</Button>
+    <Button className="float-right mr-4" href="/Register" variant="success">Create Account</Button>
     
   </Card.Body>
 </Card>
